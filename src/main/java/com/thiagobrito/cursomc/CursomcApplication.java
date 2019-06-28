79b4d1a3d5a25c6d4dc4f56b4a8112dbf72e8fd0
Thiago Brito <tbrito1986@gmail.com>
2019-06-28 15:26:28 -0300

    Pedido, estadoPagamento e Pagamento

--- src/main/java/com/thiagobrito/cursomc/CursomcApplication.java
+++ src/main/java/com/thiagobrito/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.thiagobrito.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.thiagobrito.cursomc.domain.Cidade;
 import com.thiagobrito.cursomc.domain.Cliente;
 import com.thiagobrito.cursomc.domain.Endereco;
 import com.thiagobrito.cursomc.domain.Estado;
+import com.thiagobrito.cursomc.domain.Pagamento;
+import com.thiagobrito.cursomc.domain.PagamentoComBoleto;
+import com.thiagobrito.cursomc.domain.PagamentoComCartao;
+import com.thiagobrito.cursomc.domain.Pedido;
 import com.thiagobrito.cursomc.domain.Produto;
+import com.thiagobrito.cursomc.domain.enums.EstadoPagamento;
 import com.thiagobrito.cursomc.domain.enums.TipoCliente;
 import com.thiagobrito.cursomc.repositories.CategoriaRepository;
 import com.thiagobrito.cursomc.repositories.CidadeRepository;
 import com.thiagobrito.cursomc.repositories.ClienteRepository;
 import com.thiagobrito.cursomc.repositories.EnderecoRepository;
 import com.thiagobrito.cursomc.repositories.EstadoRepository;
+import com.thiagobrito.cursomc.repositories.PagamentoRepository;
+import com.thiagobrito.cursomc.repositories.PedidoRepository;
 import com.thiagobrito.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,7 +49,13 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -65,10 +79,23 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "2423424221342", TipoCliente.PESSOAFISICA);
 		
-		cli1.getTelefones().addAll(Arrays.asList("2131231", "34234234"));
-		
 		Endereco e1 = new Endereco(null, "Rua Floreas", "400", "Apto 303", "Jardim", "232132131", cli1, c1);
 		Endereco e2 = new Endereco(null, "Rua Santa Maria", "77", "Casa", "Vila Santa Terezinha", "13131", cli1, c2);
+		
+		SimpleDateFormat stf = new SimpleDateFormat("DD/MM/YYYY HH:MM");
+		
+		Pedido ped1 = new Pedido(null, stf.parse("12/12/2019 10:23"), cli1, e1);
+		Pedido ped2 = new Pedido(null, stf.parse("12/10/2019 10:34"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, stf.parse("20/10/2019 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		cli1.getTelefones().addAll(Arrays.asList("2131231", "34234234"));
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 		
@@ -88,6 +115,9 @@ public class CursomcApplication implements CommandLineRunner{
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
 	}
 
 }
---
+++ src/main/java/com/thiagobrito/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,31 @@
+package com.thiagobrito.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.thiagobrito.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao  extends Pagamento{
+	
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+		
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
+
+}

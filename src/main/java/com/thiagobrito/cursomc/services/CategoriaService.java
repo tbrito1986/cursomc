ac9d836e7a668740e546fd115351813fdde0f1ee
Thiago Brito <tbrito1986@gmail.com>
2019-07-03 16:13:38 -0300

    Deletenado uma categoria com delete

--- src/main/java/com/thiagobrito/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagobrito/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.thiagobrito.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.thiagobrito.cursomc.domain.Categoria;
 import com.thiagobrito.cursomc.repositories.CategoriaRepository;
+import com.thiagobrito.cursomc.services.exceptions.DataIntegrityException;
 import com.thiagobrito.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao e possivel excluir uma categoria que possui produtos!");
+		}
+		
 	}
 }

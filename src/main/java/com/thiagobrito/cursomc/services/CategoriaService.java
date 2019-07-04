cbc4b8d6ddec05c18dbc7c6ed1379b91e6ea8ad5
Thiago Brito <tbrito1986@gmail.com>
2019-07-04 15:32:44 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/thiagobrito/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagobrito/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.thiagobrito.cursomc.domain.Categoria;
+import com.thiagobrito.cursomc.dto.CategoriaDTO;
 import com.thiagobrito.cursomc.repositories.CategoriaRepository;
 import com.thiagobrito.cursomc.services.exceptions.DataIntegrityException;
 import com.thiagobrito.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }

b24ea155c768446164c9bb47855e1cd880598cc9
Thiago Brito <tbrito1986@gmail.com>
2019-07-29 14:28:18 -0300

    Correções: update em CategoriaService e contrutor de pagamento

--- src/main/java/com/thiagobrito/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagobrito/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.thiagobrito.cursomc.domain.Categoria;
+import com.thiagobrito.cursomc.domain.Cliente;
 import com.thiagobrito.cursomc.dto.CategoriaDTO;
 import com.thiagobrito.cursomc.repositories.CategoriaRepository;
 import com.thiagobrito.cursomc.services.exceptions.DataIntegrityException;
@@ -34,8 +35,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -58,5 +60,9 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }

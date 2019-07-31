be51401eb1492e777b5a854510db67497eac4862
Thiago Brito <tbrito1986@gmail.com>
2019-07-31 07:32:13 -0300

    Inserindo pedido

--- src/main/java/com/thiagobrito/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/thiagobrito/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.thiagobrito.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.thiagobrito.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao  extends Pagamento{
 	
 	private Integer numeroDeParcelas;
--- src/main/java/com/thiagobrito/cursomc/resources/PedidoResource.java
+++ src/main/java/com/thiagobrito/cursomc/resources/PedidoResource.java
@@ -1,16 +1,23 @@
 package com.thiagobrito.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.thiagobrito.cursomc.domain.Categoria;
 import com.thiagobrito.cursomc.domain.Pedido;
+import com.thiagobrito.cursomc.dto.CategoriaDTO;
 import com.thiagobrito.cursomc.services.PedidoService;
 
 @RestController
@@ -24,6 +31,14 @@ public class PedidoResource {
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
